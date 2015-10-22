53201d76767aef9580000e05f97b0d64803f8aca
Joseph D Hughes <jdhughes@usgs.gov>
2015-10-22 16:35:09 -0400

    added line so build script can be run without specifying python in nix terminals.

--- pymake/build.py
+++ pymake/build.py
@@ -1,8 +1,8 @@
+#! /usr/bin/env python
 import pymake
 
 #get the arguments
 args = pymake.pymake.parser()
-print(args)
 
 #call main -- note that this form allows main to be called
 #from python as a function.
