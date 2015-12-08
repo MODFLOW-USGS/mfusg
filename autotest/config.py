8de92a936a4b026da5e9c200bf3f880236b62230
Christian D Langevin <langevin@usgs.gov>
2015-12-08 10:34:29 -0500

    again.

--- autotest/config.py
+++ autotest/config.py
@@ -21,13 +21,13 @@ testpaths = [os.path.join('..', 'examples'),
              os.path.join('..', 'test-dev')]
 srcdir = os.path.join('..', 'src')
 program = 'mfusg'
-version = '1.3.00'
+version = '1.3.01'
 target = os.path.join('temp', program + '_' + version + target_extension)
 
 # Release version information
-url_release = 'http://water.usgs.gov/ogw/mfusg/mfusg.1_2_00.zip'
-dir_release = os.path.join(testdir, 'mfusg.1_2')
+url_release = 'http://water.usgs.gov/ogw/mfusg/mfusg.1_3.zip'
+dir_release = os.path.join(testdir, 'mfusg.1_3')
 srcdir_release = os.path.join(dir_release, 'src')
-version_release = '1.2.00'
+version_release = '1.3.00'
 target_release = os.path.join('temp', program + '_' + version_release +
                               target_extension)
